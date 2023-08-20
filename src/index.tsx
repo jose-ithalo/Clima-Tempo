2f9a2757b0600bb006ca23ccb47a9a6b2a0a2113
José Ithalo <jose_ithalo@hotmail.com>
2023-08-20 17:00:58 -0300

    Alteração na renderização do componente

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/global.css';
-import App from './App';
+// import App from './App';
+import SignUp from './pages/SignIn/signUp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>
 );
 
