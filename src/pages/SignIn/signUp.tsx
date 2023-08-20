2b048917affa131d51c40dc820452c7b4cd45d1e
José Ithalo <jose_ithalo@hotmail.com>
2023-08-20 17:01:42 -0300

    Inicialização da página de cadastro

---
+++ src/pages/SignIn/signUp.tsx
@@ -0,0 +1,11 @@
+import './signUp.css';
+
+function SignUp() {
+    return (
+        <div className="container">
+            <h1>Página SignUp</h1>
+        </div>
+    );
+}
+
+export default SignUp;
