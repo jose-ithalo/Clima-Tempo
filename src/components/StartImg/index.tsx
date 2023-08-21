8c874d24041ae3bb110b1dff58c088a41ae232ad
José Ithalo <jose_ithalo@hotmail.com>
2023-08-21 12:03:24 -0300

    Desenvolvimento do componente para tela inicial

---
+++ src/components/StartImg/index.tsx
@@ -0,0 +1,11 @@
+import './startImg.css';
+
+function StartImg() {
+    return (
+        <div className="startImg">
+            <h1>ClimaTempo</h1>
+        </div>
+    );
+}
+
+export default StartImg
