53c5535617b9e0f4c251bf63b66fd5b5c36cd65a
José Ithalo <jose_ithalo@hotmail.com>
2023-08-22 16:48:33 -0300

    Desenvolvimento do formulário

--- src/components/UserSide/index.tsx
+++ src/components/UserSide/index.tsx
@@ -1,4 +1,7 @@
 import './userSide.css';
+import userIcon from '../../assets/userIcon.svg';
+import emailIcon from '../../assets/emailIcon.svg';
+import padlock from '../../assets/padlock.svg';
 
 function UserSide() {
     return (
@@ -6,12 +9,18 @@ function UserSide() {
             <h1>Crie sua conta</h1>
             <form>
                 <input type='text' placeholder='Nome completo' />
+                <img src={userIcon} alt="Ícone usuário" className='iconInput' />
+
                 <input type='email' placeholder='Email' />
+                <img src={emailIcon} alt="Ícone e-mail" className='iconInput iconInputEmail' />
+
                 <input type='password' placeholder='Senha' />
+                <img src={padlock} alt="padlock" className='iconInput' />
+
                 <button className='formButton' type='submit'>Registrar</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default UserSide;
