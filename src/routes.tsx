b8c1521d8847edea15afea2fafa6a16f711bb031
José Ithalo <jose_ithalo@hotmail.com>
2023-08-23 12:49:44 -0300

    Nova rota adicionada

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignIn";
 import Login from "./pages/Login";
+import SecondLogin from "./pages/Login/second";
 
 
 function TheRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
-      <Route path="/Login" element={<Login />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/Login" element={<SecondLogin />} />
+      <Route path="/SignUp" element={<SignUp />} />
     </Routes>
   );
 }
