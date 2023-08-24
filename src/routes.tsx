6947faf4bc1e110253c01deb213e105395dcd7ae
José Ithalo <jose_ithalo@hotmail.com>
2023-08-24 15:22:47 -0300

    Inclusão de rotas protegidas ao arquivo

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignIn";
 import Login from "./pages/Login";
 import SecondLogin from "./pages/Login/second";
+import Home from "./pages/Home";
 
+import RouterProp from "./types/routerProp";
+
+function ProtectedRoutes({ redirectTo }: RouterProp) {
+
+  const isAuthenticated: boolean = true;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+
+}
 
 function TheRoutes() {
   return (
@@ -10,6 +20,11 @@ function TheRoutes() {
       <Route path="/" element={<Login />} />
       <Route path="/Login" element={<SecondLogin />} />
       <Route path="/SignUp" element={<SignUp />} />
+
+      <Route element={<ProtectedRoutes redirectTo={'/'} />}>
+        <Route path="/Home" element={<Home />} />
+      </Route>
+
     </Routes>
   );
 }
