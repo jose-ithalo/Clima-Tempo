b2ee3223aaccc9ce1fa8ad8aec75303222ef0227
José Ithalo <jose_ithalo@hotmail.com>
2023-08-25 11:51:07 -0300

    Desenvolvimento do Side Navbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,33 @@
-
+import './home.css';
+import menuIcon from '../../assets/menuIcon.svg';
+import logout from '../../assets/logout.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+
+    const navigate = useNavigate();
+
+    function handleLogout() {
+
+        navigate('/Login');
+    }
+
     return (
-        <div>
-            <h1>Página Home</h1>
+        <div className="containerHome">
+            <div className='SideBar'>
+                <div className='navField'>
+                    <img src={menuIcon} alt="Menu" title='Menu' className='iconBar' />
+                    <img src={logout} alt="Logout" title='Sair' className='iconBar' onClick={handleLogout} />
+                </div>
+            </div>
+            <div className='contentHome'>
+                <header></header>
+                <main>
+                    <h1>Content Home</h1>
+                </main>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;
