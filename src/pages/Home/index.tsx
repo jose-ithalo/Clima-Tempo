29b91df821d1e0a51a0847cf47e6adcc4cd66065
José Ithalo <jose_ithalo@hotmail.com>
2023-08-27 12:09:59 -0300

    Desenvolvimento da imagem das informações

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import './home.css';
 import menuIcon from '../../assets/menuIcon.svg';
 import logout from '../../assets/logout.svg';
 import avatar from '../../assets/avatar.svg';
+import cloud from '../../assets/cloud.svg';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
@@ -26,7 +27,22 @@ function Home() {
                     <img src={avatar} alt="Avatar" title='Usuário logado' />
                 </header>
                 <main>
-                    <h1>Content Home</h1>
+                    <div className='fieldDetail'>
+                        <div className='leftDetails'>
+                            <div>
+                                <img src={cloud} alt='Ícone' className='iconWeather' />
+                                <h4>Chuva forte</h4>
+                            </div>
+                            <div>
+                                <h1>19°</h1>
+                                <h3>São Paulo, Brazil</h3>
+                            </div>
+                        </div>
+                        <div className='rightDetails'>
+                            <h2>7:30 pm</h2>
+                            <h3>Quarta-feira</h3>
+                        </div>
+                    </div>
                 </main>
             </div>
         </div>
