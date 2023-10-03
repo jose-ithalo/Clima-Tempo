2dd8d5c3c8234be06ad91f5f84a6c858009ebc49
José Ithalo <jose_ithalo@hotmail.com>
2023-10-03 13:19:23 -0300

    Componente para alerta de sucesso implementado

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,25 @@
+import StartImg from '../../components/StartImg';
+import UserSide from '../../components/UserSide';
+import ErrorAlert from '../../components/ErrorAlert/error';
+import SuccessAlert from '../../components/SuccessAlert/success';
+
+import { useContext } from 'react';
+import fileContext from '../../context/fileContext';
+
+function SignUp() {
+
+    const { errorContent, errorState, successState } = useContext<any>(fileContext);
+
+    return (
+        <div className="container">
+            <StartImg />
+
+            <UserSide headline='Crie sua conta' inputName={true} btnAction='Registrar' />
+
+            {errorState && <ErrorAlert content={errorContent} />}
+            {successState && <SuccessAlert />}
+        </div>
+    );
+}
+
+export default SignUp;
