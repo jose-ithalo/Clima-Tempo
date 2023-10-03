6af12f88a8e1167d762bbbe172f2bb6950fbe9fe
José Ithalo <jose_ithalo@hotmail.com>
2023-10-03 11:27:03 -0300

    Utilização da API de contexto no componete

--- src/pages/Login/second.tsx
+++ src/pages/Login/second.tsx
@@ -1,13 +1,22 @@
 import StartImg from '../../components/StartImg';
 import UserSide from '../../components/UserSide';
+import ErrorAlert from '../../components/ErrorAlert/error';
+
+import { useContext } from 'react';
+import fileContext from '../../context/fileContext';
 
 function SecondLogin() {
+
+    const { errorContent, errorState } = useContext<any>(fileContext);
+
     return (
         <div className="container">
             <StartImg />
 
             <UserSide headline='Olá, novamente!' inputName={false}
                 btnAction='Entrar' passForget={true} />
+
+            {errorState && <ErrorAlert content={errorContent} />}
         </div>
     );
 }
