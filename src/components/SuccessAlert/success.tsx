2bc3464e9ec59981dd87479e15d69cc326b9ff71
José Ithalo <jose_ithalo@hotmail.com>
2023-10-03 13:15:42 -0300

    Componente para alerta de sucesso criado

---
+++ src/components/SuccessAlert/success.tsx
@@ -0,0 +1,9 @@
+function SuccessAlert() {
+    return (
+        <div className='popupAlert' style={{ backgroundColor: '#09ad09' }}>
+            Usuário cadastrado com sucesso!
+        </div>
+    );
+}
+
+export default SuccessAlert;
