6334043447e879af635e99ca1b1accfd64ebec27
José Ithalo <jose_ithalo@hotmail.com>
2023-11-04 10:57:15 -0300

    Criação de componente Loading

---
+++ src/components/Loading/loading.tsx
@@ -0,0 +1,25 @@
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { useState } from 'react';
+
+function Loading() {
+    const [open, setOpen] = useState<boolean>(true);
+
+    setTimeout(() => {
+        setOpen(false);
+    }, 4000);
+
+    return (
+        <div>
+            <Backdrop
+                sx={{ backgroundColor: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+            >
+                <CircularProgress color="secondary" />
+            </Backdrop>
+        </div>
+    );
+}
+
+export default Loading;
