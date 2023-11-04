1c47479c97662812063eec96c5e27b3a1ad03302
José Ithalo <jose_ithalo@hotmail.com>
2023-11-04 10:58:49 -0300

    Aplicação do loading na página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,7 @@ import plus from '../../assets/plus.svg';
 import CardWeather from '../../components/CardWeather';
 import ModalSearch from '../../components/ModalSearch';
 import ErrorAlert from '../../components/ErrorAlert/error';
+import Loading from '../../components/Loading/loading';
 
 import api from '../../services/apiBase';
 
@@ -19,6 +20,7 @@ function Home() {
     const { modalState, setModalState, errorState, navigate } = useContext<any>(fileContext);
 
     const [cityList, setCityList] = useState<string[]>([]);
+    const [stateLoading, setStateLoading] = useState<boolean>(true);
 
     async function showCities(): Promise<void> {
 
@@ -45,54 +47,62 @@ function Home() {
 
     useEffect(() => {
         showCities();
+        setTimeout(() => {
+            setStateLoading(false);
+        }, 3000);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div className="containerHome">
-            <div className='sideBar'>
-                <img src={menuIcon} alt="Menu" title='Menu' className='iconBar' />
-                <img src={logout} alt="Logout" title='Sair' className='iconBar' onClick={handleLogout} />
-            </div>
-            <div className='contentHome'>
-                <header>
-                    <img src={avatar} alt="Avatar" title='Usuário logado' />
-                </header>
-                <main>
-                    <section className='fieldDetail'>
-                        <div className='leftDetails'>
-                            <div>
-                                <img src={cloud} alt='Ícone' className='iconWeather' />
-                                <h4>Chuva forte</h4>
-                            </div>
-                            <div>
-                                <h1>19°</h1>
-                                <h3>São Paulo, Brazil</h3>
-                            </div>
-                        </div>
-                        <div className='rightDetails'>
-                            <h2>7:30 pm</h2>
-                            <h3>Quarta-feira</h3>
-                        </div>
-                    </section>
-                    <section className='fieldOption'>
-                        <div className='upperContent'>
-                            <h3>Temperaturas</h3>
-                            <button className='addButton' onClick={() => setModalState(true)}>
-                                <img src={plus} alt="Sinal de mais" />
-                                Adicionar cidade
-                            </button>
-                        </div>
-                        <div className='weatherReports'>
-                            {cityList.map((city, index) => (
-                                <CardWeather key={index} cityName={city} />
-                            ))}
-                        </div>
-                    </section>
-                </main>
-            </div>
-            {modalState && <ModalSearch />}
-            {errorState && <ErrorAlert content='Erro ao carregar dados da cidade' />}
+            {stateLoading ? <Loading /> :
+                <>
+                    <div className='sideBar'>
+                        <img src={menuIcon} alt="Menu" title='Menu' className='iconBar' />
+                        <img src={logout} alt="Logout" title='Sair' className='iconBar' onClick={handleLogout} />
+                    </div>
+                    <div className='contentHome'>
+                        <header>
+                            <img src={avatar} alt="Avatar" title='Usuário logado' />
+                        </header>
+                        <main>
+                            <section className='fieldDetail'>
+                                <div className='leftDetails'>
+                                    <div>
+                                        <img src={cloud} alt='Ícone' className='iconWeather' />
+                                        <h4>Chuva forte</h4>
+                                    </div>
+                                    <div>
+                                        <h1>19°</h1>
+                                        <h3>São Paulo, Brazil</h3>
+                                    </div>
+                                </div>
+                                <div className='rightDetails'>
+                                    <h2>7:30 pm</h2>
+                                    <h3>Quarta-feira</h3>
+                                </div>
+                            </section>
+                            <section className='fieldOption'>
+                                <div className='upperContent'>
+                                    <h3>Temperaturas</h3>
+                                    <button className='addButton' onClick={() => setModalState(true)}>
+                                        <img src={plus} alt="Sinal de mais" />
+                                        Adicionar cidade
+                                    </button>
+                                </div>
+                                <div className='weatherReports'>
+                                    {cityList.map((city, index) => (
+                                        <CardWeather key={index} cityName={city} />
+                                    ))}
+                                </div>
+                            </section>
+                        </main>
+                    </div>
+                    {modalState && <ModalSearch />}
+                    {errorState && <ErrorAlert content='Erro ao carregar dados da cidade' />}
+                </>
+            }
+
         </div>
     );
 }
