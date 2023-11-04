8cf59717cb5dbb997107d49ca38c7ec086ea399d
José Ithalo <jose_ithalo@hotmail.com>
2023-11-04 12:59:55 -0300

    Função para remover cidade desenvolvida

--- src/components/DelButton/button.tsx
+++ src/components/DelButton/button.tsx
@@ -1,10 +1,42 @@
 import './button.css';
 import delIcon from '../../assets/deleteIcon.svg'
 
-function DelButton() {
+import CityProp from '../../types/cityProp';
+
+import api from '../../services/apiBase';
+
+import fileContext from '../../context/fileContext';
+import { useContext } from 'react';
+
+function DelButton({ cityName }: CityProp) {
+
+    const { setErrorDelete } = useContext<any>(fileContext);
+
+    async function removeCity() {
+
+        try {
+            const token: string | null = localStorage.getItem('token');
+            await api.delete('/users/cities', {
+                headers: {
+                    authorization: `Bearer ${token}`
+                },
+                data: {
+                    city: cityName
+                },
+            });
+
+            window.location.reload();
+        } catch (error) {
+            setErrorDelete(true);
+
+            setTimeout(() => {
+                setErrorDelete(false);
+            }, 5000);
+        }
+    }
 
     return (
-        <div className='deleteButton'>
+        <div className='deleteButton' title='Remover cidade' onClick={removeCity}>
             <img src={delIcon} alt='Botão X' />
         </div>
     );
