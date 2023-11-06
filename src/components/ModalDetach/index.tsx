f9f9a3d4e63b822710faf207783901f61be14a14
José Ithalo <jose_ithalo@hotmail.com>
2023-11-06 17:22:06 -0300

    Criação do modal para destacar cidade

---
+++ src/components/ModalDetach/index.tsx
@@ -0,0 +1,19 @@
+import './modal.css';
+
+function ModalDetach() {
+    return (
+        <div className='modalDetach'>
+            <i className="fa-solid fa-xmark"></i>
+            <section className='detachSection'>
+                <h2>Deseja destacar essa cidade ?</h2>
+                <button className='btnDetach'>Destacar</button>
+            </section>
+            <section>
+                <h2>Deseja excluir essa cidade ?</h2>
+                <button className='btnDelete'>Excluir</button>
+            </section>
+        </div>
+    )
+}
+
+export default ModalDetach;
