e491140977810fb1533ab7a91833983113c051b8
José Ithalo <jose_ithalo@hotmail.com>
2023-11-07 16:31:42 -0300

    Container para o modal inserido

--- src/components/ModalDetach/index.tsx
+++ src/components/ModalDetach/index.tsx
@@ -2,16 +2,18 @@ import './modal.css';
 
 function ModalDetach() {
     return (
-        <div className='modalDetach'>
-            <i className="fa-solid fa-xmark"></i>
-            <section className='detachSection'>
-                <h2>Deseja destacar essa cidade ?</h2>
-                <button className='btnDetach'>Destacar</button>
-            </section>
-            <section>
-                <h2>Deseja excluir essa cidade ?</h2>
-                <button className='btnDelete'>Excluir</button>
-            </section>
+        <div className='containerDetach'>
+            <div className='modalDetach'>
+                <i className="fa-solid fa-xmark"></i>
+                <section className='detachSection'>
+                    <h2>Deseja destacar essa cidade ?</h2>
+                    <button className='btnDetach'>Destacar</button>
+                </section>
+                <section>
+                    <h2>Deseja excluir essa cidade ?</h2>
+                    <button className='btnDelete'>Excluir</button>
+                </section>
+            </div>
         </div>
     )
 }
