da279e7f936ddfa62a8aaa3529a5c22a7e37756c
José Ithalo <jose_ithalo@hotmail.com>
2023-11-07 17:08:52 -0300

    Adição de evento no botão fechar

--- src/components/ModalDetach/index.tsx
+++ src/components/ModalDetach/index.tsx
@@ -1,10 +1,17 @@
 import './modal.css';
 
+import fileContext from '../../context/fileContext';
+
+import { useContext } from 'react';
+
 function ModalDetach() {
+
+    const { setDetachState } = useContext<any>(fileContext);
+
     return (
         <div className='containerDetach'>
             <div className='modalDetach'>
-                <i className="fa-solid fa-xmark"></i>
+                <i className="fa-solid fa-xmark" onClick={() => setDetachState(false)}></i>
                 <section className='detachSection'>
                     <h2>Deseja destacar essa cidade ?</h2>
                     <button className='btnDetach'>Destacar</button>
