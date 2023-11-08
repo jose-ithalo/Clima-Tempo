8a6586968fa7a3c045bb45bdc43a6df669810638
José Ithalo <jose_ithalo@hotmail.com>
2023-11-08 12:42:20 -0300

    Função para remover cidade desenvolvida

--- src/components/ModalDetach/index.tsx
+++ src/components/ModalDetach/index.tsx
@@ -1,12 +1,38 @@
 import './modal.css';
 
+import ErrorAlert from '../ErrorAlert/error';
+
+import api from '../../services/apiBase';
 import fileContext from '../../context/fileContext';
 
 import { useContext } from 'react';
 
 function ModalDetach() {
 
-    const { setDetachState } = useContext<any>(fileContext);
+    const { chosenCity, errorState, setErrorState, setDetachState } = useContext<any>(fileContext);
+
+    async function removeCity() {
+        try {
+            const token: string | null = localStorage.getItem('token');
+            await api.delete('/users/cities', {
+                headers: {
+                    authorization: `Bearer ${token}`
+                },
+                data: {
+                    city: chosenCity
+                },
+            });
+            window.location.reload();
+
+        } catch (error) {
+            setErrorState(true);
+
+            setTimeout(() => {
+                setErrorState(false)
+            }, 3000);
+        }
+
+    }
 
     return (
         <div className='containerDetach'>
@@ -18,9 +44,10 @@ function ModalDetach() {
                 </section>
                 <section>
                     <h2>Deseja excluir essa cidade ?</h2>
-                    <button className='btnDelete'>Excluir</button>
+                    <button className='btnDelete' onClick={removeCity}>Excluir</button>
                 </section>
             </div>
+            {errorState && <ErrorAlert content='Erro ao remover cidade' />}
         </div>
     )
 }
