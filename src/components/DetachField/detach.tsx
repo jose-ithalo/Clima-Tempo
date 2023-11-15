03943ccf2b3276666458ae50e5d0c67e2078507c
José Ithalo <jose_ithalo@hotmail.com>
2023-11-15 10:36:34 -0300

    Desenvolvimento do componente de destaque

--- src/components/DetachField/detach.tsx
+++ src/components/DetachField/detach.tsx
@@ -1,9 +1,24 @@
 import './detach.css';
 
+import cloud from '../../assets/cloud.svg';
+
 function DetachField() {
     return (
-        <div className='test'>
-            Destaque
+        <div className='fieldDetach'>
+            <div className='leftDetails'>
+                <div>
+                    <img src={cloud} alt='Ícone' className='iconWeather' />
+                    <h4>Chuva forte</h4>
+                </div>
+                <div>
+                    <h1>19°</h1>
+                    <h3>São Paulo, Brazil</h3>
+                </div>
+            </div>
+            <div className='rightDetails'>
+                <h2>7:30 pm</h2>
+                <h3>Quarta-feira</h3>
+            </div>
         </div>
     )
 }
