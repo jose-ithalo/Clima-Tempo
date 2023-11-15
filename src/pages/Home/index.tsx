8157cffeeed8e42bd5dbc7b0ee0a936b90bfbaac
José Ithalo <jose_ithalo@hotmail.com>
2023-11-15 10:33:36 -0300

    Mudança de elementos para Componente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,6 @@ import './home.css';
 import menuIcon from '../../assets/menuIcon.svg';
 import logout from '../../assets/logout.svg';
 import avatar from '../../assets/avatar.svg';
-import cloud from '../../assets/cloud.svg';
 import plus from '../../assets/plus.svg';
 
 import DetachField from '../../components/DetachField/detach';
@@ -76,25 +75,11 @@ function Home() {
                             <img src={avatar} alt="Avatar" title='Usuário logado' />
                         </header>
                         <main>
-                            <section className='fieldDetail'>
-                                <div className='leftDetails'>
-                                    <div>
-                                        <img src={cloud} alt='Ícone' className='iconWeather' />
-                                        <h4>Chuva forte</h4>
-                                    </div>
-                                    <div>
-                                        <h1>19°</h1>
-                                        <h3>São Paulo, Brazil</h3>
-                                    </div>
-                                </div>
-                                <div className='rightDetails'>
-                                    <h2>7:30 pm</h2>
-                                    <h3>Quarta-feira</h3>
-                                </div>
+                            <section>
+                                <DetachField />
                             </section>
                             <section className='fieldOption'>
                                 <div className='upperContent'>
-                                    <DetachField />
                                     <h3>Temperaturas</h3>
                                     {errorDelete && <p className='deleteError'>Erro na exclusão</p>}
                                     <button className='addButton' onClick={() => setModalState(true)}>
