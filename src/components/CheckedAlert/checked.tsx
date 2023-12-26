ba109c5df7ec353e323c340c676a2203ede48c14
José Ithalo <jose_ithalo@hotmail.com>
2023-12-26 11:40:13 -0300

    Desenvolvimento do componente

--- src/components/CheckedAlert/checked.tsx
+++ src/components/CheckedAlert/checked.tsx
@@ -1,9 +1,11 @@
-
+import './checked.css';
+import checkedImg from '../../assets/checked.svg';
 
 function Checked() {
     return (
-        <div className="checkedContainer">
-            Tudo ok
+        <div className="checked">
+            <h1>Em instantes você receberá um email.<br />Aguarde.</h1>
+            <img className='checkedImg' src={checkedImg} alt="Imagem de ok" />
         </div>
     )
 }
