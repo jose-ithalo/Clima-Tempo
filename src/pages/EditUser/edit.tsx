cd08292fb4df6dc8ce2a5418a5da16ff6839530e
José Ithalo <jose_ithalo@hotmail.com>
2024-01-11 12:15:24 -0300

    Criação de página para alteração de dados

---
+++ src/pages/EditUser/edit.tsx
@@ -0,0 +1,14 @@
+import './edit.css';
+
+function EditUser() {
+
+    return (
+        <div className="editContainer">
+            <div className='editCard'>
+                Editar
+            </div>
+        </div>
+    )
+}
+
+export default EditUser;
