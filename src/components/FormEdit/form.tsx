fc56de45807963829e83c295c910efec06fac5db
José Ithalo <jose_ithalo@hotmail.com>
2024-01-13 12:58:45 -0300

    Remoção de div

--- src/components/FormEdit/form.tsx
+++ src/components/FormEdit/form.tsx
@@ -6,29 +6,27 @@ import padlock from '../../assets/padlock.svg';
 function FormEdit() {
     return (
         <div className='editCard'>
-            <div className="formEdit">
-                <h2>Alteração de dados</h2>
-                <form>
-                    <div className='editInput'>
-                        <input type="text" placeholder='Novo nome' />
-                        <img src={userIcon} alt="Ícone usuário" className='iconInput' />
-                    </div>
+            <h2>Alteração de dados</h2>
+            <form className='formEdit'>
+                <div className='editInput'>
+                    <input type="text" placeholder='Novo nome' />
+                    <img src={userIcon} alt="Ícone usuário" className='iconInput' />
+                </div>
 
-                    <div className='editInput'>
-                        <input type="text" placeholder='Novo email' />
-                        <img src={emailIcon} alt="Ícone e-mail" className='iconInput iconInputEmail' />
-                    </div>
+                <div className='editInput'>
+                    <input type="text" placeholder='Novo email' />
+                    <img src={emailIcon} alt="Ícone e-mail" className='iconInput iconInputEmail' />
+                </div>
 
-                    <div className='editInput'>
-                        <input type="password" placeholder='Nova senha' />
-                        <img src={padlock} alt="padlock" className='iconInput' />
-                    </div>
+                <div className='editInput'>
+                    <input type="password" placeholder='Nova senha' />
+                    <img src={padlock} alt="padlock" className='iconInput' />
+                </div>
 
-                    <button className='formButton' type='button'>
-                        Alterar
-                    </button>
-                </form>
-            </div>
+                <button className='formButton' type='button'>
+                    Alterar
+                </button>
+            </form>
         </div>
     )
 }
