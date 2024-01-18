517b8046148abfc98e1feb401447143f74aca84d
José Ithalo <jose_ithalo@hotmail.com>
2024-01-18 12:03:20 -0300

    Props para componente adicionadas

--- src/pages/EditUser/edit.tsx
+++ src/pages/EditUser/edit.tsx
@@ -1,11 +1,16 @@
 import './edit.css';
 import FormEdit from '../../components/FormEdit/form';
 
+import { useContext } from 'react';
+import fileContext from '../../context/fileContext';
+
 function EditUser() {
+
+    const { userData } = useContext<any>(fileContext);
 
     return (
         <div className="editContainer">
-            <FormEdit />
+            <FormEdit userName={userData.username} userEmail={userData.email} />
         </div>
     )
 }
