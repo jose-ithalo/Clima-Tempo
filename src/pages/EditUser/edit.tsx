63ee54c3a0c41171d08fde440ff835179c480a2b
José Ithalo <jose_ithalo@hotmail.com>
2024-01-21 09:35:07 -0300

    Importação do componente

--- src/pages/EditUser/edit.tsx
+++ src/pages/EditUser/edit.tsx
@@ -1,16 +1,18 @@
 import './edit.css';
 import FormEdit from '../../components/FormEdit/form';
+import SuccessAlert from '../../components/SuccessAlert/success';
 
 import { useContext } from 'react';
 import fileContext from '../../context/fileContext';
 
 function EditUser() {
 
-    const { userData } = useContext<any>(fileContext);
+    const { userData, successState } = useContext<any>(fileContext);
 
     return (
         <div className="editContainer">
             <FormEdit userName={userData.username} userEmail={userData.email} />
+            {successState && <SuccessAlert content='Os dados foram atualizados' />}
         </div>
     )
 }
