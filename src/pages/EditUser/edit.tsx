cce81af408eca17ddbcef4cf99f0998960277684
José Ithalo <jose_ithalo@hotmail.com>
2024-01-22 13:23:43 -0300

    Importação do ErrorAlert

--- src/pages/EditUser/edit.tsx
+++ src/pages/EditUser/edit.tsx
@@ -1,18 +1,20 @@
 import './edit.css';
 import FormEdit from '../../components/FormEdit/form';
 import SuccessAlert from '../../components/SuccessAlert/success';
+import ErrorAlert from '../../components/ErrorAlert/error';
 
 import { useContext } from 'react';
 import fileContext from '../../context/fileContext';
 
 function EditUser() {
 
-    const { userData, successState } = useContext<any>(fileContext);
+    const { userData, successState, errorContent, errorState } = useContext<any>(fileContext);
 
     return (
         <div className="editContainer">
             <FormEdit userName={userData.username} userEmail={userData.email} />
             {successState && <SuccessAlert content='Os dados foram atualizados' />}
+            {errorState && <ErrorAlert content={errorContent} />}
         </div>
     )
 }
