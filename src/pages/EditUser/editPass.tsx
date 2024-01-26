e0873f8eea912a2832d8435826eb52dcc9bc2d08
José Ithalo <jose_ithalo@hotmail.com>
2024-01-26 15:52:27 -0300

    Página criada para redefinição de senha

---
+++ src/pages/EditUser/editPass.tsx
@@ -0,0 +1,22 @@
+import './edit.css';
+import FormEdit from '../../components/FormEdit/form';
+import SuccessAlert from '../../components/SuccessAlert/success';
+import ErrorAlert from '../../components/ErrorAlert/error';
+
+import { useContext } from 'react';
+import fileContext from '../../context/fileContext';
+
+function EditPass() {
+
+    const { successState, errorContent, errorState } = useContext<any>(fileContext);
+
+    return (
+        <div className="editContainer">
+            <FormEdit resetInput={true} />
+            {successState && <SuccessAlert content='Os dados foram atualizados' />}
+            {errorState && <ErrorAlert content={errorContent} />}
+        </div>
+    )
+}
+
+export default EditPass;
