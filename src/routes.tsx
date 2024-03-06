d42896e0920a133c8ab91423e2dd111437f37c2f
José Ithalo <jose_ithalo@hotmail.com>
2024-03-05 21:11:39 -0300

    Remoção do estado userData

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,7 +12,6 @@ import EditPass from "./pages/EditUser/editPass";
 
 import RouterProp from "./types/routerProp";
 import Weather from "./types/weather";
-import User from "./types/user";
 
 function ProtectedRoutes({ redirectTo }: RouterProp) {
 
@@ -46,18 +45,12 @@ function TheRoutes() {
     humidity: 0
   });
 
-  const [userData, setUserData] = useState<User>({
-    id: 0,
-    username: '',
-    email: ''
-  });
-
   return (
     <fileContext.Provider value={
       {
         errorState, setErrorState, errorContent, setErrorContent, successState, setSuccessState,
-        modalState, setModalState, weatherData, setWeatherData, userData, setUserData, errorDelete,
-        setErrorDelete, detachState, setDetachState, chosenCity, setChosenCity, deletedCity,
+        modalState, setModalState, weatherData, setWeatherData, errorDelete, setErrorDelete,
+        detachState, setDetachState, chosenCity, setChosenCity, deletedCity,
         setDeletedCity, sentMail, setSentMail, navigate
       }
     }>
