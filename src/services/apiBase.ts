6ea2e4d1b6401e2ade24f7c535c8c1db7d25f21f
José Ithalo <jose_ithalo@hotmail.com>
2025-01-14 11:59:51 -0300

    Alteração na URL e da API

--- src/services/apiBase.ts
+++ src/services/apiBase.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api-clima-tempo.cyclic.app',
+    baseURL: 'https://api-clima-tempo-production.up.railway.app',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });
